94dcd8b40c983a23e92a901deb43b38505a476be
kedrakk <keaindrak@gmail.com>
2022-05-07 00:22:39 +0630

    late int

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -9,6 +9,9 @@ import android.widget.Toast
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage:ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,12 +19,11 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener(){
             rollDice()
         }
+        diceImage=findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
-        val randomInt=Random.nextInt(6)+1
-        val diceImage:ImageView=findViewById(R.id.dice_image)
-        val drawableResource=when(randomInt){
+        val drawableResource=when(Random.nextInt(6)+1){
             1->R.drawable.dice_1
             2->R.drawable.dice_2
             3->R.drawable.dice_3
